34d902c8be0869cd17b9dfa9dbb3e52c3095c90f
Katrin <katrin_bennington@mail.ru>
2024-06-04 09:28:29 +0500

    restore .idea and add requirements

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '15',
+        'USER': 'posgres',
+        'PORT': '5432',
+        'PASSWORD': 'iLmF-13.',
     }
 }
 
